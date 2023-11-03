d8cfa127a5226d38c70e767adabfceb55913d95f
Rafael Montaño <boraita@gmail.com>
2023-11-03 14:11:50 +0100

    Update const.py

--- custom_components/rointe/const.py
+++ custom_components/rointe/const.py
@@ -15,7 +15,7 @@ CONF_INSTALLATION = "rointe_installation"
 
 ROINTE_MANUFACTURER = "Rointe"
 
-ROINTE_SUPPORTED_DEVICES = ["radiator", "towel", "therm", "radiatorb", "oval_towel"]
+ROINTE_SUPPORTED_DEVICES = ["radiator", "towel", "therm", "radiatorb", "oval_towel", "acs"]
 
 CMD_SET_TEMP = "cmd_set_temp"
 CMD_SET_PRESET = "cmd_set_preset"
